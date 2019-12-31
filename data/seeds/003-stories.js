1e65f11926ed84ae54700e18791ab718e56c2194
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-12-31 23:02:17 +0100

    added story again

--- data/seeds/003-stories.js
+++ data/seeds/003-stories.js
@@ -1,132 +1,123 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex("stories").insert([
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "One",
-          story:
-            ""
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Two",
-          story:
-            "Bizimana"
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Three",
-          story:
-            "Yara has love”"
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Three",
-          story:
-            "Yara has love”"
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Four",
-          story:
-            "I am r home has been destroyed by the rebels."
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Five",
-          story:
-            "We niyah City."
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Six",
-          story:
-            "It is very hadon’t know if they’re alive or not."
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Seven",
-          story:
-            "During the factional warool."
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Eight",
-          story:
-            "Abdul now lives in as to drive buses, likes to help others, and loves soccer."
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Nine",
-          story:
-            "Abdul now lives in an a"
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Ten",
-          story:
-            "Yara has loved sewing since shad to flee Syria,” she explained. Her parents and one of her sisters were a refugee camp in Jordan, and two sisters still in Syria. “I worry about them every day.”"
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Eleven",
-          story:
-            "I am a widow who had eight children.home has been destroyed by the rebels."
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Twelve",
-          story:
-            "We hid ourselves on the mountain iet region in Northern Iraq. From there, we drove to Sulaymaniyah City."
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Thirteen",
-          story:
-            "It ors of 11 years. I don’t know if they’re alive or not."
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Fourteen",
-          story:
-            "During the factional war, photo from AAE’s school."
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Fifteen",
-          story:
-            "Abdul now lives in an ap"
-        },
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Sixteen",
-          story:
-            "Abdul now lives in an apartmen"
-        }
-        ,
-        {
-          source:
-            "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
-          title: "Seventeen",
-          story:
-            "Abdul now lives in an apartmen"
-        }
-      ]);
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "One",
+      story: ""
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Two",
+      story: "Bizimana"
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Three",
+      story: "Yara has love”"
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Three",
+      story: "Yara has love”"
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Four",
+      story: "I am r home has been destroyed by the rebels."
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Five",
+      story: "We niyah City."
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Six",
+      story: "It is very hadon’t know if they’re alive or not."
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Seven",
+      story: "During the factional warool."
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Eight",
+      story:
+        "Abdul now lives in as to drive buses, likes to help others, and loves soccer."
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Nine",
+      story: "Abdul now lives in an a"
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Ten",
+      story:
+        "Yara has loved sewing since shad to flee Syria,” she explained. Her parents and one of her sisters were a refugee camp in Jordan, and two sisters still in Syria. “I worry about them every day.”"
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Eleven",
+      story:
+        "I am a widow who had eight children.home has been destroyed by the rebels."
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Twelve",
+      story:
+        "We hid ourselves on the mountain iet region in Northern Iraq. From there, we drove to Sulaymaniyah City."
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Thirteen",
+      story: "It ors of 11 years. I don’t know if they’re alive or not."
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Fourteen",
+      story: "During the factional war, photo from AAE’s school."
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Fifteen",
+      story: "Abdul now lives in an ap"
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Sixteen",
+      story: "Abdul now lives in an apartmen"
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Seventeen",
+      story: "Abdul now lives in an apartmen"
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Seventeen",
+      story: "Abdul now lives in an apartmen"
+    }
+  ]);
 };
