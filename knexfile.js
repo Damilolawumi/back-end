6e772e0376307b1bec89d85b4d3d678c6ab4f9d4
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-01-01 14:42:14 +0100

    switched DB

--- knexfile.js
+++ knexfile.js
@@ -40,18 +40,37 @@ module.exports = {
       }
     }
   },
+  // production: {
+  //   client: "pg",
+  //   connection: process.env.DATABASE_URL,
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     directory: "./data/migrations"
+  //   },
+  //   seeds: {
+  //     directory: "./data/seeds/"
+  //   }
+  // }
   production: {
-    client: "pg",
-    connection: process.env.HEROKU_POSTGRESQL_BROWN_URL,
-    pool: {
-      min: 2,
-      max: 10
+    client: "sqlite3",
+    connection: {
+      filename: "./data/stories_db.db3"
     },
+    useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations"
     },
     seeds: {
-      directory: "./data/seeds/"
+      directory: "./data/seeds"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
     }
-  }
+  },
+  
 };
