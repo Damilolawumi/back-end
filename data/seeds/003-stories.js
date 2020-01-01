cda190e5e59cfd9a5022477775f2de398996457c
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-01-01 09:38:31 +0100

    removed unneccesary spaces

--- data/seeds/003-stories.js
+++ data/seeds/003-stories.js
@@ -2,120 +2,103 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex("stories").insert([
     {
-      source:
-        " ",
+      source: "",
       title: "First Date; Photoshoot",
       story:
         "I have never had an encounter with someone and connected instantly like I did with you. Anyone around that day wouldn’t believe that was our first time meeting eahc other. A very unconventional first date; with other people with us, a photoshoot. I’m glad we don’t have the regular first date story like other people ☺️"
     },
     {
-      source:
-        " ",
+      source: "",
       title: "15th February, 2017",
       story:
         "It’s funny how we had a lot of arguments after almost a year of being together, trying to figure out the day we ‘officially’ started dating. We had a lot of options but nothing really stuck, until we both agreed it had to be the 14th of February (A day before you took this picture). I haven’t loved you any less since then."
     },
     {
-      source:
-        " ",
+      source: "",
       title: "Summer Body!",
       story:
         "Since I met you, I’ve never known any place to have bad vibes with you around. You always find a way to light up the room. You’ve kept my life lit all through, and for this I say, Thankyou. "
     },
     {
-      source:
-        " ",
+      source: "",
       title: "Sun Kissed!",
       story:
         "On side, it’s funny how , one of the reasons I got serious with taking bomb ass pictures is ‘cos of you. I mean, I gotta know your angles so I can show the world the love of my life. I know how difficult it is to get you smile and actually show your teeth lol. Remember how I got you to do just that on this day?"
     },
     {
-      source:
-        " ",
+      source: "",
       title: "Rho’s JPEG",
       story:
         "You make a lot of sacrifices for and I never get to say thank you enough. I mean you could have done anything you wanted but you chose to stay diplomatic on this day for me. Thank you ❤️ "
     },
     {
-      source:
-        " ",
+      source: "",
       title: "Thank you ❤️",
       story:
         "This year you were there for me especially when I was broken. You kept up with my problems this year and I didn’t even make it any easier.   Thanks for being an absolutely amazing person ☺️"
     },
     {
-      source:
-        " ",
+      source: "",
       title: "Me and Mine",
       story:
         "I think this is one of the biggest events we went to together. This day really pointed out how much you put me in mind when we have to be somewhere; trying to make sure I am comfortable ❤️. This is unofficially our favorite picture together, for the year. Unofficially because you didn’t pick anyone in particular but you picked this day. "
     },
     {
-      source:
-        " ",
+      source: "",
       title: "Okama",
       story:
         "You are beautiful and I’m always grateful to call you mine. Thank you for loving me 🥰🥰"
     },
     {
-      source:
-        " ",
+      source: "",
       title: "April 7, 2019",
       story:
         "A lot of people/ friends can’t vibe with other people's partners but I’m always proud when everyone around me enjoys having you around. Like, yea, y'all can now understand why I’m with her. On this particular occasion you went out of your way to help me drag people to come for the event , even when you didn’t need to."
     },
     {
-      source:
-        " ",
+      source: "",
       title: "Sept 12; Our Other Birthday.",
       story:
         "I am in love with the best person. No one can tell me otherwise. You always take it upon yourself to make sure I have a memorable birthday, EVERY SINGLE YEAR. Thank you for making my birthday a memorable one. Thank you for being a part of my life. ☺️"
     },
     {
-      source:
-        " ",
+      source: "",
       title: "Baby Boy 😂",
       story:
         "This is the one time you decided to cut your hair just like mine 😂.2019 was our most trying year so far. A lot happened that could have broken us but you still chose to stay. Sometimes I feel like I don’t even deserve you. I love you. Thank you for not giving up."
     },
     {
-      source:
-        " ",
+      source: "",
       title: "Ribena Amb.",
       story:
         "This is an appreciation post; for always bringing the vibe wherever you go and of course for always staying beautiful ❤️"
     },
     {
-      source:
-        " ",
+      source: "",
       title: "Happy Birthday Baby ❤️",
       story:
         "You put in a lot of effort making sure I’m not left alone to drown in my thoughts. You’re my support system and knowing I have you? Now that gives me a lot of hope. "
     },
     {
-      source:
-        " ",
+      source: "",
       title: "The Sun ☀️",
       story:
         "This day further serves as evidence we are our own vibe. You made the yellow dress look good. Every single thing you touch just becomes beautiful. Thank you for being in my life ❤️"
     },
     {
-      source:
-        " ",
+      source: "",
       title: "The Oracle and his.",
       story:
         "I love this picture of us together. One of the days we had a lot of fun. I’m always glad to have you be part   Of the beautiful memories I have.❤️"
     },
     {
-      source:
-        " ",
+      source: "",
       title: "Wedding Guests.",
       story:
         "So Amongst the many talents I have, one of them is always finding a way to get on your last nerve for almost all the events we go together. This day was not an exception. Funny how we almost missed taking this picture together and Olabisi told me “ Put everything aside, You’ll regret not taking pictures with her after today”. I’m glad I listened.  I never got to tell you how beautiful you looked on this day. 🥰"
     },
     {
-      source:
-        " ",
+      source: "",
       title: "Prvncess",
       story:
         "I’m probably the most annoying person, always forcing you to take pictures of yourself for me. Somehow, you always always no matter the situation, and your mood, making sure I get JPEGs 🥰 especially for events I miss. ❤️"
