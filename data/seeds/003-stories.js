db2535d1e2f24230c1e3a0fc05c6e4852d9969c9
damilola.olawumi <damilola_olawumi@yahoo.com>
2020-01-02 23:49:32 +0100

    few changes made

--- data/seeds/003-stories.js
+++ data/seeds/003-stories.js
@@ -2,106 +2,128 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex("stories").insert([
     {
-      source: "",
-      title: "Happy Birthday Baby ❤️",
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "First Date",
       story:
-        "I’m probably the most annoying person, always forcing you to take pictures of yourself for me. Somehow, you always always no matter the situation, and your mood, making sure I get JPEGs 🥰 especially for events I miss. ❤️"
+        "I remember this day cause we went to your/(our) favourite restaurant for a meal and we bet i wouldnt be able to finish that fish, i took you on that bet and failed cause it was a lot for me but i had fun trying to win you. It was the begining of everything "
     },
     {
-      source: "",
-      title: "Wedding Guests.",
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Favorite Grin",
       story:
-        "So Amongst the many talents I have, one of them is always finding a way to get on your last nerve for almost all the events we go together. This day was not an exception. Funny how we almost missed taking this picture together and Olabisi told me “ Put everything aside, You’ll regret not taking pictures with her after today”. I’m glad I listened.  I never got to tell you how beautiful you looked on this day. 🥰"
+        "We were supposed to go on a date on this day, so we got excited and got dressed then decided it was a selfie type of day... as lady luck would have it rain started poring heavilly and Lekki Conservation Center decided to close cause of the heavy rain. It didnt stop us from going out cause we were determined to have fun, so we went to Rufus and Bees and it was Dope AF"
     },
     {
-      source: "",
-      title: "The Oracle and his.",
-      story:
-        "I love this picture of us together. One of the days we had a lot of fun. I’m always glad to have you be part   Of the beautiful memories I have.❤️"
-    },
-    {
-      source: "",
-      title: "The Sun ☀️",
-      story:
-        "This day further serves as evidence we are our own vibe. You made the yellow dress look good. Every single thing you touch just becomes beautiful. Thank you for being in my life ❤️"
-    },
-    {
-      source: "",
-      title: "👑 Prvncess 🍯 💦 🍦💐",
-      story:
-        "You put in a lot of effort making sure I’m not left alone to drown in my thoughts. You’re my support system and knowing I have you? Now that gives me a lot of hope. "
-    },
-    {
-      source: "",
-      title: "Ribena Amb.",
-      story:
-        "This is an appreciation post; for always bringing the vibe wherever you go and of course for always staying beautiful ❤️"
-    },
-    {
-      source: "",
-      title: "Baby Boy 😂",
-      story:
-        "This is the one time you decided to cut your hair just like mine 😂.2019 was our most trying year so far. A lot happened that could have broken us but you still chose to stay. Sometimes I feel like I don’t even deserve you. I love you. Thank you for not giving up."
-    },
-    {
-      source: "",
-      title: "Sept 12; Our Other Birthday.",
-      story:
-        "I am in love with the best person. No one can tell me otherwise. You always take it upon yourself to make sure I have a memorable birthday, EVERY SINGLE YEAR. Thank you for making my birthday a memorable one. Thank you for being a part of my life. ☺️"
-    },
-    {
-      source: "",
-      title: "April 7, 2019",
-      story:
-        "A lot of people/ friends can’t vibe with other people's partners but I’m always proud when everyone around me enjoys having you around. Like, yea, y'all can now understand why I’m with her. On this particular occasion you went out of your way to help me drag people to come for the event , even when you didn’t need to."
-    },
-    {
-      source: "",
-      title: "👑 Okama 🍦",
-      story:
-        "You are beautiful and I’m always grateful to call you mine. Thank you for loving me 🥰🥰"
-    },
-    {
-      source: "",
-      title: "Me and Mine",
-      story:
-        "I think this is one of the biggest events we went to together. This day really pointed out how much you put me in mind when we have to be somewhere; trying to make sure I am comfortable ❤️. This is unofficially our favorite picture together, for the year. Unofficially because you didn’t pick anyone in particular but you picked this day. "
-    },
-    {
-      source: "",
-      title: "Thank you ❤️",
-      story:
-        "This year you were there for me especially when I was broken. You kept up with my problems this year and I didn’t even make it any easier.   Thanks for being an absolutely amazing person ☺️"
-    },
-    {
-      source: "",
-      title: "Rho’s JPEG",
-      story:
-        "You make a lot of sacrifices for me and I never get to say thank you enough. I mean you could have done anything you wanted but you chose to stay diplomatic on this day for me. Thank you ❤️ "
-    },
-    {
-      source: "",
-      title: "Sun Kissed!",
-      story:
-        "On side, it’s funny how , one of the reasons I got serious with taking bomb ass pictures is ‘cos of you. I mean, I gotta know your angles so I can show the world the love of my life. I know how difficult it is to get you to smile and actually show your teeth lol. Remember how I got you to do just that on this day?"
-    },
-    {
-      source: "",
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
       title: "Summer Body!",
       story:
-        "Since I met you, I’ve never known any place to have bad vibes with you around. You always find a way to light up the room. You’ve kept my life lit all through, and for this I say, Thank you. "
+        "So this was the first gym we registered together, its funny how we went together every time even though we had the busiest and craziest work schedules… made me realize that even though we were busy we made time to work out together… and it was even funnier that we quit at the same time...lol "
     },
     {
-      source: "",
-      title: "15th February, 2017",
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Favourite Smile",
       story:
-        "It’s funny how we had a lot of arguments after almost a year of being together, trying to figure out the day we ‘officially’ started dating. We had a lot of options but nothing really stuck, until we both agreed it had to be the 14th of February (A day before you took this picture). I haven’t loved you any less since then."
+        "Your smile makes me smile, and I remember asking for a selfie from you and you sent this picture, it actually still makes me smile because I realized this smile right here is enough to brighten my day."
     },
     {
-      source: "",
-      title: "First Date; Photoshoot",
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Facetime",
       story:
-        "I have never had an encounter with someone and connected instantly like I did with you. Anyone around that day wouldn’t believe that was our first time meeting each other. A very unconventional first date; a photoshoot. I’m glad we don’t have the regular first date story like other people ☺️"
-    }
+        "We were supposed to go out this day and I was dressed and at work, I asked if you were ready and you were like Yeah you’ve left the house… I knew you weren’t ready so i called you via facetime and you totally forgot and picked and then when you realized what happened we just laughed about it..."
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Birthday",
+      story:
+        "That Friday we went for a birthday dinner in a fancy restaurant and we felt really cute and decided it would be nice to take a picture and we did and we loved it."
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Me and Mine",
+      story:
+        "You have always been there for me, supporting me, cheering me up and giving me words of advice. When I informed you that i wanted to quit my job and start programming you were soo excited, you told me you believed in me and said you’ve got my back and have been a wonderful support system for me... I appreciate you a lot. "
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Cutie",
+      story:
+        "On this day we went for falz’s concert at eko hotel, and you looked so cute and innocent and I couldn’t resist but take a picture of you. 🥰🥰"
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Best Guy",
+      story:
+        "It was my sister’s wedding and I was sooo stressed, I had a lot of responsibilities that day and couldn’t attend to my guests that came for the wedding so you told me you’ve got me... you took my phone and helped get all my guests seated in the venue and attended to their needs like I would have. This was the only picture we took together that day and I cherish it a lot."
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Te amo",
+      story:
+        "Just when I think that it is impossible to love you any more than I already do, you prove me wrong."
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Beach Day ",
+      story:
+        "We went for a beach hangout and it was fun… I remember someone asking if whale was fish or meat and having a lot of weird conversations… and the best part of that day was spending the whole day with you... and at that time this was your sexy face pose...lmfao"
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Bae",
+      story:
+        "When we took the picture it was about Six months into the relationship, and then I knew you were the most important person in my life. Funny how I never stopped thinking about you. I want to let you know I love you with each passing day. ❤️"
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Nkem❤️",
+      story:
+        "I’m happy you’re mine. Thanks for being there for me, listening to me, consoling me, caring for me, spoiling me, respecting me loving me etc… I bless the day I met you. "
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Bestie",
+      story:
+        "I never knew I could find a man who would be all I wanted and more. And in so such a short time, you became my everything. You are the love of my life and my best and truest friend. ❤️"
+    },
+    {
+      source:
+        "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+      title: "Happy Birthday Babe 🥰",
+      story:
+        "I still ponder on how well you understand me; both on the days my hormones make me crazy and the days I’m struggling to be myself. I will never forget about the moment I realized that I loved you.❤️"
+    },
+    // {
+    //   source:
+    //     "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+    //   title: "Wedding Guests. ",
+    //   story:
+    //     "So Amongst the many talents I have, one of them is always finding a way to get on your last nerve for almost all the events we go together. This day was not an exception. Funny how we almost missed taking this picture together and Olabisi told me “ Put everything aside, You’ll regret not taking pictures with her after today”. I’m glad I listened.  I never got to tell you how beautiful you looked on this day. 🥰"
+    // },
+    // {
+    //   source:
+    //     "https://medium.com/globalgoodness/12-powerful-refugee-stories-from-around-the-world-5c0a54d2e2ed",
+    //   title: "Prvncess",
+    //   story:
+    //     "I’m probably the most annoying person, always forcing you to take pictures of yourself for me. Somehow, you always always no matter the situation, and your mood, making sure I get JPEGs 🥰 especially for events I miss. ❤️"
+    // }
   ]);
 };
+
+
+
+
+
